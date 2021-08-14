72995c58de71ea0fc50c0cd7b893a28ded0406f2
ythosa <vasus714@yandex.ru>
2021-08-14 03:43:30 +0300

    base app

---
+++ src/test/kotlin/com/ythosa/simplespring/app/ws/SimpleSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ythosa.simplespring.app.ws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
